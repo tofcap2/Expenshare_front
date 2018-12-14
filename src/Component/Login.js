378fee8309ef002784bf0f02098fa361df334240
tofcap2 <cap2family@hotmail.fr>
2018-12-14 16:46:05 +0100

    Création des components

---
+++ src/Component/Login.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {} from 'reactstrap';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
